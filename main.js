fe3ce10cac35b704c1b4cf2a5c035f9909483847
John Doyle <johndoyle@gmail.com>
2020-01-05 16:19:36 -0500

    Finished application

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-console.log("Enabled!!!!!!!!!!!!!");
 
 document.getElementById('lbsInput').addEventListener('input', function (e) {
     let pounds = e.target.value;
-    console.log(pounds);
+    let grams = pounds/0.022046;
+    document.getElementById('gramsOutput').innerHTML = grams;
+    document.getElementById('kgOutput').innerHTML = grams/1000;
+    document.getElementById('ozOutput').innerHTML = pounds * 16
 });
 
